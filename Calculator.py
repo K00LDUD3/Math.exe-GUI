7164949aca79e29e3bb767f8aaa70ad7fcbfee03
K00LDUD3 <50181316+K00LDUD3@users.noreply.github.com>
2022-01-10 23:40:59 +0530

    Created generic functions for BUTTON, LABEL, ENTRY

--- Calculator.py
+++ Calculator.py
@@ -8,9 +8,18 @@ root = Tk()
 root.title('Calculator')
 
 class Calc:
+    widg = None
     text = ''
     wd = {} #Widget Dictionary
-    gd = {
+    gd = {}
+    #frame = None
+    def __init__(self, type, widg_dict, text, grid_dict) -> None:
+        #Assigning param variables to class variables
+        self.text = text
+        self.wd = widg_dict
+        self.gd = grid_dict
+        '''
+        gd = {
             'column':None,
             'row':None,
             'cspan':None,
@@ -20,31 +29,33 @@ class Calc:
             'ipadx':None,
             'ipady':None
         }
-    #frame = None
-    def __init__(self, type, widg_dict, text, grid_dict) -> None:
-        self.text = text
-        self.wd = widg_dict
-        #check type of widg (button, label, textbox, frame), then call a function to create that specific widg
+        '''
+
+        #checking type of widg (button, label, textbox, frame), then call a function to create that specific widg
         if type.upper() == 'BUTTON':
+            self.widg = Button()
             pass
         elif type.upper() == 'LABEL':
+            self.widg = Label()
             pass
-        elif type.upper() == 'TEXTBOX':
+        elif type.upper() == 'ENTRY':
+            self.widg = Entry()
             pass
-        elif type.upper() == 'FRAME':
+        elif type.upper() == 'LABELFRAME':
+            self.widg = LabelFrame()
             pass
         else:
             print('err: widget unidentified')
             return
         pass
-    
-    #Gen 
+
+    #Generating a BUTTON using parameters stored in a dictionary
     def createB(self):
-        self.button = Button(
-            master= self.wd['frame'],
+        self.widg = Button(
+            master= self.wd['master'],
+            text=self.text,
             activebackground= self.wd['act_bg'],
             activeforeground= self.wd['act_fg'],
-            text=self.text,
             width=self.wd['w'],
             height=self.wd['height'],
             font=self.wd['font'],
@@ -59,20 +70,19 @@ class Calc:
             underline=self.wd['underline']
 
         )
-        self.button.grid(row=0, column=0)
-
-
-
-
-
-
-
-
-
-frame = LabelFrame(root)
-frame.pack()
-
-widg_test_dict = {'frame':frame,
+        self.widg.grid(
+            column= self.gd['column'],
+            row= self.gd['row'],
+            columnspan= self.gd['cspan'],
+            rowspan= self.gd['rspan'],
+            padx= self.gd['padx'],
+            pady= self.gd['pady'],
+            ipadx= self.gd['ipadx'],
+            ipady= self.gd['ipady']
+        )
+        '''
+        button_widg_dict = {
+            'master':Nones,
             'act_bg':None,
             'act_fg':None,
             'bg':None,
@@ -87,9 +97,154 @@ widg_test_dict = {'frame':frame,
             'pady':None,
             'relief':None,
             'underline':None,
-            'w':10,
-            'wraplength':None}
-widg_test = Calc('BUTTON', widg_test_dict, 'test', None)
-widg_test.createB()
+            'w':None,
+            'wraplength':None
+        }
+        '''
+        return
+
+    #Generating a LABEL using parameters stored in a dictionary
+    def createL(self):
+        self.widg = Label(
+            master= self.wd['master'],
+            textvariable=  self.text,
+            anchor=  self.wd['anchor'],
+            background=  self.wd['bg'],
+            foreground=  self.wd['fg'],
+            bitmap=  self.wd['bitmap'],
+            bd=  self.wd['bd'],
+            font=  self.wd['font'],
+            height=  self.wd['height'],
+            image=  self.wd['image'],
+            justify=  self.wd['justify'],
+            padx=  self.wd['padx'],
+            pady=  self.wd['pady'],
+            relief=  self.wd['relief'],
+            underline=  self.wd['underline'],
+            width=  self.wd['w'],
+            wraplength=  self.wd['wraplength']
+        )
+        self.widg.grid(
+            column= self.gd['column'],
+            row= self.gd['row'],
+            columnspan= self.gd['cspan'],
+            rowspan= self.gd['rspan'],
+            padx= self.gd['padx'],
+            pady= self.gd['pady'],
+            ipadx= self.gd['ipadx'],
+            ipady= self.gd['ipady']
+        )
+        '''
+        lab_dict = {
+            'master':None,
+            'anchor':None,
+            'bg':None,
+            'bitmap':None,
+            'bd':None,
+            'font':None,
+            'fg':None,
+            'height':None,
+            'image':None,
+            'justify':None,
+            'padx':None,
+            'pady':None,
+            'relief':None,
+            'text':None,
+            'textvar':None,
+            'underline':None,
+            'w':None,
+            'wraplength':None
+        }   
+        '''
+        return
+
+    #Generating a ENTRY (aka textbox) using parameters stored in a dictionary
+    def createE(self):
+        self.widg = Entry(
+            master= self.wd['master'],
+            textvariable= self.text,
+            width= self.wd['w'],
+            bd= self.wd['bd'],
+            background= self.wd['bg'],
+            foreground= self.wd['fg'],
+            font= self.wd['font'],
+            insertofftime= self.wd['insertofftime'],
+            insertontime= self.wd['insesrtontime'],
+            highlightbackground= self.wd['highlbg'],
+            highlightcolor= self.wd['highlcolor'],
+            relief= self.wd['relief'],
+            highlightthickness= self.wd['highthick'],
+            cursor= self.wd['cursor'],
+            xscrollcommand= self.wd['xscrollcommand']
+        )
+        self.widg.grid(
+            column= self.gd['column'],
+            row= self.gd['row'],
+            columnspan= self.gd['cspan'],
+            rowspan= self.gd['rspan'],
+            padx= self.gd['padx'],
+            pady= self.gd['pady'],
+            ipadx= self.gd['ipadx'],
+            ipady= self.gd['ipady']
+        )
+        '''
+        entry_dict = {
+            'master':None,
+            'bd':None,
+            'height':None,
+            'width':None,
+            'bg':None,
+            'fg':None,
+            'font':None,
+            'insertofftime':None,
+            'insertontime':None,
+            'padx':None,
+            'pady':None,
+            'highthick':None,
+            'charwidth':None,
+            'relief':None,
+            'yscrollcommand':None,
+            'xscrollcommand':None,
+        }
+        '''
+        return
+
+'''-------------TESTING-------------'''
+frame = LabelFrame(root)
+frame.pack()
+
+widg_test_dict = {
+    'master':frame,
+    'act_bg':None,
+    'act_fg':None,
+    'bg':None,
+    'fg':None,
+    'border':None,
+    'font':None,
+    'height':None,
+    'highl_color':None,
+    'image':None,
+    'justify':None,
+    'padx':None,
+    'pady':None,
+    'relief':None,
+    'underline':None,
+    'w':10,
+    'wraplength':None
+}
+            
+grid_test_dict = {
+    'column':0,
+    'row':1,
+    'cspan':1,
+    'rspan':1,
+    'padx':0,
+    'pady':0,
+    'ipadx':0,
+    'ipady':5
+}
+widg_test = Calc(type='button', widg_dict=widg_test_dict, text='test', grid_dict=grid_test_dict)
+widg_test.createB() 
+print(str(widg_test.widg).split('!')[-1]) # getting type of widget
 
 root.mainloop()

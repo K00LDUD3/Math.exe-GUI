672d16b4560d00ebe8228fddffe41c297eae7895
K00LDUD3 <50181316+K00LDUD3@users.noreply.github.com>
2022-01-10 13:19:33 +0530

    huh

--- Calculator.py
+++ Calculator.py
@@ -22,9 +22,8 @@ class Calc:
     #frame = None
     def __init__(self, type, widg_dict, text, grid_dict) -> None:
         #check type of widg (button, label, textbox, frame), then call a function to create that specific widg
-        self.wd = widg_dict
         if type.upper() == 'BUTTON':
-            self.createB(self.wd, text)
+            self.b_dict_init(widg_d = widg_dict)
             pass
         elif type.upper() == 'LABEL':
             pass
@@ -42,7 +41,7 @@ class Calc:
         return
 
     def b_dict_init(self, widg_d):
-        self.d = {
+        self.wd = {
             #button config params
             'act_bg':widg_d['act_bg'],
             'act_fg':widg_d['act_fg'],
@@ -61,6 +60,9 @@ class Calc:
             'w':widg_d['w'],
             'wraplength':widg_d['wraplength'],
         }
+
+
+        
         
 
 root.mainloop()
